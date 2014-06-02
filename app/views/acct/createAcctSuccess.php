057f2fb1f615dca3ec721d4ce56de0d887a543f1
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2014-06-02 01:03:39 -0400

    Modify Success Page

--- app/views/acct/createAcctSuccess.php
+++ app/views/acct/createAcctSuccess.php
@@ -4,7 +4,7 @@
     <div class="login-panel panel panel-default">
         <div class="panel-heading"><h3 class="text-center">Account Successfully Created!</h3></div>
         <div class="panel-body text-center">
-            <p>You successfully linked your device to your online portal account! Please check your email for a confirmation letter. <br/>If you do not see the email, please check your spam folder and mark it as a 'non-spam' message.</p>
+            <p>You successfully linked your device to your online portal account! Please check your email for a confirmation letter. <br/><br>/If you do not see the email, please check your spam folder and mark it as a 'non-spam' message.</p>
         </div>
     </div>
     </div>
